4bd44d443b9993f29c6ea81061c0bfcc8157b0a5
OlexandraKovach2 <105677310+OlexandraKovach2@users.noreply.github.com>
2023-03-04 15:58:00 +0300

    lab1

---
+++ lab1/laravel-app/resources/views/rez.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+</head>
+<body>
+<div>
+    <h3>Контактна інформація:</h3>
+    <ul>
+        <li>Телефон: +380957100780</li>
+        <li>Name:Sashka</li>
+    </ul>
+
+    <h3> Навички:</h3>
+    <ul>
+        <li>Розробка проектів на Laravel</li>
+        <li>Робота з базами даних MySQL</li>
+        <li>Знання HTML, CSS, JavaScript</li>
+        <li>Вміння працювати з Git</li>
+    </ul>
+
+    <h3>Досвід роботи:</h3>
+    <ul>
+        <li>Стажер-програміст в IT-компанії "Компанія Х" (2021 рік)</li>
+        <li>Розробка та підтримка веб-сайтів на Laravel</li>
+        <li>Налагодження та підтримка баз даних MySQL</li>
+        <li>Взаємодія з командою розробників та керівниками проектів</li>
+    </ul>
+
+    <h3>Проекти:</h3>
+    <ul>
+        <li>Розробка веб-сайту для магазину "Магазин Х" на Laravel (2022 рік)</li>
+        <li>Розробка з нуля з використанням Laravel та MySQL</li>
+        <li>Використання HTML, CSS, JavaScript для створення інтерфейсу користувача</li>
+        <li>Налагодження та оптимізація бази даних</li>
+        <li>Використання Git для контролю версій</li>
+    </ul>
+
+
+       <h3>Освіта:</h3>
+        <ul>
+            <li>Бакалавр програмування, Національний технічний університет України "КПІ" (2018-2022)</li>
+        </ul>
+    <h3>Мови програмування:</h3>
+    <ul>
+        <li>PHP</li>
+        <li>JavaScript</li>
+    </ul>
+
+    <h3>Інші навички:</h3>
+    <ul>
+        <li>Вміння працювати в команді</li>
+        <li>Англійська мова</li>
+    </ul>
+</div>
+</body>
+</html>
